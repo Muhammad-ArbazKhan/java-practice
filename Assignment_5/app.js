5011c76b39dcdcd91e65ba6c5cfbf862fd72529b
Muhammad Arbaz Khan <piyareearbaz@gmail.com>
2025-12-21 11:22:01 +0500

    update

---
+++ Assignment_5/app.js
@@ -0,0 +1,104 @@
+// Q1
+// var multiarry[]
+// var multiarry [[],[],[]]
+// Q2
+// var matrix [0, 1, 2, 3]
+//            [1, 0, 1, 2]
+//            [2, 1, 0, 1]
+// Q3
+// for(var i = 1; i <=10; i++){
+    // document.write(i + "<br>");
+// }
+// Q4
+// var tableNumber = +prompt("Enter your fav number")
+// var tableLength = +prompt("Enter your table length")
+
+// for(var i = 1; i <= tableLength; i++)[
+//     document.write(
+//         tableNumber + " * " + i + " = " + (tableNumber * i ) + "<br>"
+//     )
+// ]
+// Q5
+// var fruit = ["apple", "banana" ,"orange", "mango", "grabs"]
+// for(i = 1; i < fruit.length; i++)[
+//     document.write(fruit[i] + "<br>")
+// ]
+// Q6
+// a
+// document.write("<h1> Counting: </h1>")
+
+// for (i = 1; i <=15 ; i++)[
+//     document.write(i +  "," )
+// ]
+// // b
+// document.write("<h1> Revers Counting: </h1>")
+// for (i = 15; i >=1 ; i--)[
+//     document.write(i +  "," )
+// ]
+// // c
+// document.write("<h1> Even Number: </h1>")
+// for (i = 0; i <= 20 ; i+= 2)[
+//     document.write(i +  "," )
+// ]
+// // d
+// document.write("<h1> Odd Number: </h1>")
+// for (i = 1; i <= 20 ; i+= 2)[
+//     document.write(i +  "," )
+// ]
+// // e
+// document.write("<h1> Series Number: </h1>")
+// for (i = 2; i <= 20 ; i+= 2)[
+//     document.write(i +  "k" )
+// ]
+// Q7
+// var sweets = prompt("Wel to Hafiz sweets ;What do you want order sir/mam")
+// var userinput = ["cake", "apple pie", "cookie", "chips", "patties"]
+// userInput = userInput.toLowerCase();
+
+//   var found = false;
+// for (i = 0 ; i < A.lenght ; i++){
+//     if(A[i] === userinput)
+//         found = true
+//     break;
+// }
+// if(found){
+//     alert (userinput + "available at index " + i + "in our sweets" )
+// }
+// else{
+//     alert ("we are sorry " + userinput + "it;s not available sweets")
+// }
+  // var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+  // // user se input lena
+  // var userInput = prompt("Welcome to ABC Bakery. What do you want to order?");
+  // userInput = userInput.toLowerCase();
+
+  // var found = false;
+
+  // for (var i = 0; i < A.length; i++) {
+  //   if (A[i] === userInput) {
+  //     found = true;
+  //     break;
+  //   }
+  // }
+
+  // if (found) {
+  //   alert(userInput + " is available at index " + i + " in our bakery.");
+  // } else {
+  //   alert("We are sorry. " + userInput + " is not available in our bakery.");
+  // }
+
+// Q8 
+// var A = ['24', '53', '78', '91', '12']
+// var largest = Math.max(...A
+// )
+// console.log("Array", A);
+
+// console.log("largest", largest)
+// Q9
+// var A = ['24', '53', '78', '91', '12']
+// var smallest = Math.max(...A
+// )
+// console.log("Array", A);
+
+// console.log("smallest", smallest)
